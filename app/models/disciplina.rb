62672bba01c2d383e15fe9ebc3634f108c29e0c4
EdvaldoHLeite <edvaldoh.leite@gmail.com>
2021-03-07 20:04:18 -0300

    Validação de disciplina

--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -1,4 +1,19 @@
 class Disciplina < ApplicationRecord
   belongs_to :turma
   belongs_to :user
+
+  validates :nome,
+            presence: true,
+            length: {
+              minimum: 4,
+              maximum: 20,
+              message: "Tamanho do nome de 4-20 caracteres"}
+  validates :anoLetivo,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 2020,
+              message: "Ano letivo a partir de 2020" }
+  validates :turma_id, presence: true
+  validates :user_id, presence: true
 end
