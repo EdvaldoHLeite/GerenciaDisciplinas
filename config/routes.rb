044ec0000f751d08ef771d99b07e72f6b5c0d766
EdvaldoHLeite <edvaldoh.leite@gmail.com>
2021-03-07 08:39:17 -0300

    Formulário singup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :users
   root 'welcome#home'
+
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
+  delete "/sessions", to: "sessions#destroy"
 end
