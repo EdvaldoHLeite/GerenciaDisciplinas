9fa22b2410acb4beeecc2b3fe722eef19d6c6405
EdvaldoHLeite <edvaldoh.leite@gmail.com>
2021-03-07 18:16:11 -0300

    Teste de model de turma

--- app/models/turma.rb
+++ app/models/turma.rb
@@ -1,21 +1,5 @@
 class Turma < ApplicationRecord
-  validates :letra,
-            presence: true,
-            format: { with: /\A[a-zA-Z]+\z/, message: "Somente uma letra"},
-            length: 1
-  validates :anoLetivo,
-            presence: true,
-            numericality: {
-              only_integer: true,
-              greater_than_or_equal_to: 2020,
-              message: "Ano letivo a partir de 2020"
-            }
-  validates :ano,
-            presence: true,
-            numericality: {
-              only_integer: true,
-              greater_than_or_equal_to: 1,
-              less_than_or_equal_to: 9,
-              message: "Ano (serie) de 1º a 9º"
-            }
+  validates :letra, presence: true, length: {is:1}, format: { with: /\A[a-zA-Z]+\z/, message: "Somente uma letra"}
+  validates :anoLetivo, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 2020, message: "Ano letivo a partir de 2020" }
+  validates :ano, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 9, message: "Ano (serie) de 1º a 9º" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :login,
-            presence: true,
-            uniqueness: true,
-            length: {minimum: 5,
-                     maximum: 20,
-                     message: "Login com tamanho de 5 a 20"}
-  validates :password,
-            presence: true,
-            length: {minimum: 6,
-                     maximum: 15,
-                     message: "Senha com tamanho de 6 a 15"}
+  validates :login, presence: true, uniqueness: true, length: {minimum: 5, maximum: 20, message: "Login com tamanho de 5 a 20"}
+  validates :password, presence: true, length: {minimum: 6, maximum: 15, message: "Senha com tamanho de 6 a 15"}
 end
