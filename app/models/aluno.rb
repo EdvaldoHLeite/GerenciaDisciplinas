8074b5ea3d7ca38196bbcc842b24ab6fa2486c5f
EdvaldoHLeite <edvaldoh.leite@gmail.com>
2021-03-08 14:21:21 -0300

    validação dos campos de aluno

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,3 +1,5 @@
 class Aluno < ApplicationRecord
   belongs_to :turma
+  validates :nome, presence: true, length: {minimum: 5, maximum: 20, message: "Tamanho do nome de 5-20 caracteres"}
+  validates :turma_id, presence: true
 end
