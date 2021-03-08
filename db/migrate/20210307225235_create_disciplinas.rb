49c43c4ed095a29f2770955d83785042de5ada2a
EdvaldoHLeite <edvaldoh.leite@gmail.com>
2021-03-07 21:30:47 -0300

    Testes de model para disciplina

--- db/migrate/20210307225235_create_disciplinas.rb
+++ db/migrate/20210307225235_create_disciplinas.rb
@@ -5,6 +5,8 @@ class CreateDisciplinas < ActiveRecord::Migration[6.1]
       t.integer :anoLetivo
       t.belongs_to :turma, null: false, foreign_key: true
       t.belongs_to :user, null: false, foreign_key: true
+      #t.integer :turma_id
+      #t.integer :user_id
 
       t.timestamps
     end
--- test/models/disciplina_test.rb
+++ test/models/disciplina_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class DisciplinaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Disciplina criada corretamente' do
+    turma = Turma.new letra: 'A', anoLetivo: '2020', ano: '5'
+    turma.save
+    user = User.new login: 'marcos', password: '123456', password_confirmation: '123456'
+    user.save
+
+    disciplina = Disciplina.new nome: 'Inglês', anoLetivo: '2020', turma_id: turma.id, user_id: user.id
+    assert disciplina.save
+  end
+
+  test 'disciplina com nome vazio' do
+    turma = Turma.new letra: 'C', anoLetivo: '2020', ano: '5'
+    turma.save
+    user = User.new login: 'joaquim', password: '123456', password_confirmation: '123456'
+    user.save
+
+    disciplina = Disciplina.new nome: "", anoLetivo: '2020', turma_id: turma.id, user_id: user.id
+    assert_not disciplina.save
+  end
+
+  test 'disciplina com ano letivo menor que o especificado' do
+    turma = Turma.new letra: 'C', anoLetivo: '2020', ano: '5'
+    turma.save
+    user = User.new login: 'joaquim', password: '123456', password_confirmation: '123456'
+    user.save
+
+    disciplina = Disciplina.new nome: "Matemática", anoLetivo: '2009', turma_id: turma.id, user_id: user.id
+    assert_not disciplina.save
+  end
 end
