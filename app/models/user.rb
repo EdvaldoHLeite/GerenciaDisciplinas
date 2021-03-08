8b1b94ef4d7f1c2c66e757105d863f9d72c175af
EdvaldoHLeite <edvaldoh.leite@gmail.com>
2021-03-08 09:06:29 -0300

    Atualizando relacionamentos de disciplina

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :disciplinas
+  has_many :disciplinas, dependent: :destroy
   validates :login, presence: true, uniqueness: true, length: {minimum: 5, maximum: 20, message: "Login com tamanho de 5 a 20"}
   validates :password, presence: true, length: {minimum: 6, maximum: 15, message: "Senha com tamanho de 6 a 15"}
 end
